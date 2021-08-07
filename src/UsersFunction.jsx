bf2041daa43c9041f6f66952e47e55fd36c23ff8
Simony <anyomisimon@gmail.com>
2021-08-07 19:41:39 +0000

    function component api call

--- src/UsersFunction.jsx
+++ src/UsersFunction.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 const UsersFunction = () => {
+    let myUsers = [];
+    const [users, setUsers] = useState(myUsers);
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/users").then( responds => {
+            console.log(responds);
+            setUsers(responds.data)
+        }).catch(err => {
+            console.log(err);
+        })
+
+    })
     return (
+        <>
         <div>
-            
+          <h1>All Users</h1>  
         </div>
+        <hr />
+        <div>
+            {users.map(user => <div>
+                <p>{user.name}</p>
+                <p>{user.username}</p>
+                <p>{user.email}</p>
+
+                <hr />
+            </div>)}
+        </div>
+        </>
+    
     );
 }
 
